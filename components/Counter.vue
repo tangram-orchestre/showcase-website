b0415ef9eba661b16bf43a05a8fcfb2fc9ff47b7
Luc Varoqui <luc@varoqui.org>
2024-08-17 14:51:42 +0200

    Initial commit

---
+++ components/Counter.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const val = ref(0);
+</script>
+
+<template>
+    <h1>{{ val }}</h1>
+    <button @click="val += 1">Increment</button>
+    <button @click="val -= 1">Decrement</button>
+</template>
