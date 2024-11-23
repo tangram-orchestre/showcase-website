4299ba26b47a26ff7d4a43a939dcc7af01a62e29
Luc Varoqui <luc@varoqui.org>
2024-11-23 23:27:38 +0100

    add shadown to cagnotte

--- components/TheCagnotte.vue
+++ components/TheCagnotte.vue
@@ -19,25 +19,23 @@
           Pour continuer à faire vivre ce projet, nous avons besoin de
           <strong>votre soutien</strong>&nbsp;!
         </p>
-        <ul>
-          <li
-            class="mt-6 grid grid-cols-1 items-center gap-y-4 rounded-3xl bg-slate-900 p-4 text-white md:grid-cols-5"
+        <div
+          class="mt-6 grid grid-cols-1 items-center gap-y-4 rounded-3xl bg-slate-900 p-4 text-white shadow-xl md:grid-cols-5"
+        >
+          <a
+            class="lilita-one-regular col-span-2 block rounded-lg border-2 bg-[#81ccb5] px-8 py-4 text-center text-3xl text-black hover:bg-[#8adac2]"
+            href="https://www.helloasso.com/associations/tangram/formulaires/2"
           >
-            <a
-              class="lilita-one-regular col-span-2 block rounded-lg border-2 bg-[#81ccb5] px-8 py-4 text-center text-3xl text-black hover:bg-[#8adac2]"
-              href="https://www.helloasso.com/associations/tangram/formulaires/2"
-            >
-              Faites un don
-            </a>
-            <div class="col-span-3">
-              <p class="text-center text-sm italic md:px-6 md:text-base">
-                Chaque contribution, même modeste, nous aide à financer nos
-                <strong>répétitions</strong>, <strong>concerts</strong> et
-                <strong>projets</strong>.
-              </p>
-            </div>
-          </li>
-        </ul>
+            Faites un don
+          </a>
+          <div class="col-span-3">
+            <p class="text-center text-sm italic md:px-6 md:text-base">
+              Chaque contribution, même modeste, nous aide à financer nos
+              <strong>répétitions</strong>, <strong>concerts</strong> et
+              <strong>projets</strong>.
+            </p>
+          </div>
+        </div>
         <div class="mt-8 flex justify-center">
           <p
             class="lilita-one-regular max-w-sm text-center text-2xl md:max-w-max"
