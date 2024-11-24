6b4e22b2c7e9b33b8cb615c2db73b451ea88751b
Luc Varoqui <luc@varoqui.org>
2024-11-24 18:57:27 +0100

    move seo to app.vue

--- app.vue
+++ app.vue
@@ -1,4 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    lang: "fr",
+  },
+});
+
+useSeoMeta({
+  title: "Tangram Orchestre",
+  ogTitle: "My Amazing Site",
+  ogUrl: "https://www.tangram-orchestre.fr",
+  description: "Orchestre amateur pour Chef⸱fe⸱s en herbe",
+  ogDescription: "Orchestre amateur pour Chef⸱fe⸱s en herbe",
+  ogImage: "http://www.tangram-orchestre.fr/images/partoches.jpg",
+  twitterCard: "summary_large_image",
+});
+</script>
 
 <template>
   <NuxtLayout>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,3 @@
-<script setup lang="ts">
-useHead({
-  htmlAttrs: {
-    lang: "fr",
-  },
-});
-
-useSeoMeta({
-  title: "Tangram Orchestre",
-  ogTitle: "My Amazing Site",
-  ogUrl: "https://www.tangram-orchestre.fr",
-  description: "Orchestre amateur pour Chef⸱fe⸱s en herbe",
-  ogDescription: "Orchestre amateur pour Chef⸱fe⸱s en herbe",
-  ogImage: "http://www.tangram-orchestre.fr/images/partoches.jpg",
-  twitterCard: "summary_large_image",
-});
-</script>
-
 <template>
   <Head>
     <Title>Tangram Orchestre</Title>
